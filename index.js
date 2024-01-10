0868e7b3c3ecb2430008abb04bb09337c9743e13
sapir6888 <146127167+sapir6888@users.noreply.github.com>
2024-01-10 12:39:59 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -8,3 +8,14 @@ var image2 = "./images/dice"+ randomNumber2 + ".png";
 document.getElementsByClassName("img1")[0].src=image1
 document.getElementsByClassName("img2")[0].src=image2
 
+if(randomNumber1 > randomNumber2){
+    document.getElementsByTagName("h1")[0].innerHTML="Player 1 Won";
+}
+
+else if(randomNumber2 > randomNumber1){
+    document.getElementsByTagName("h1")[0].innerHTML="Player 2 Won";
+}
+
+else if(randomNumber1=randomNumber2){
+    document.getElementsByTagName("h1")[0].innerHTML="It's a draw!";
+}
